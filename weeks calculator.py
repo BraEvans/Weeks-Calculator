60f7fc6312939a738a5b77c5ce957c9e04128a03
Square <88930488+BraEvans@users.noreply.github.com>
2024-06-25 15:29:11 +0000

    Add files via upload

---
+++ weeks calculator.py
@@ -0,0 +1,7 @@
+print ("Do you want to calculate your weeks left")
+age = input("please input your age below\n")
+age_output= int(age)
+years_remianing = 90 - age_output
+weeks_remaining = 4 * 12 * years_remianing
+print (f"Well, You have {weeks_remaining} weeks left.\n")
+print("live your life to the fullest.\n Thanks😊")
